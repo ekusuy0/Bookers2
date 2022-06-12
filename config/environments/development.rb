1a9ed24506751dd6528a8c3bea467457daca0b82
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-06-12 13:19:17 +0000

    [Update] ActiveStoreageでimageを持たせる

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "1e1ec3c3c6724a10bf963f596bb22a4a.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
 
 end
