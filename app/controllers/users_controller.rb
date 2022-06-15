ff5888ddb3aa95b7f13c2907ebef2f145b735119
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-06-15 08:36:40 +0000

    [Update] userのeditページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,21 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
-  def create
+  def edit
+    @user = User.find(params[:id])
   end
 
-  def edit
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:title, :introduction, :profile_image)
+  end
 
 
 
