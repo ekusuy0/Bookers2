17ce64940fbe18b1acd0b9e1c520fdeb93e407cb
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-06-15 08:48:59 +0000

    [Update] Update機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,13 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    redirect_to user_path(@user)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:title, :introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 
