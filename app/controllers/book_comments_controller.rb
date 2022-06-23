9e5a3a2268b5647a2c4848b4245c3fda7e930d4f
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-06-23 04:45:39 +0000

    [Update] コメント機能の非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,11 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = @book.id
     comment.save
-    @books = Book.find(params[:book_id])
     @book_comment = BookComment.new
   end
 
   def destroy
-    @books = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
     @book_comment.destroy
     @book_comment = BookComment.new
