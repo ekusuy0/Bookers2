028d90d7cf1d10174dc03276bcf9145f39cff231
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-06-24 08:22:24 +0000

    [Update] 過去一週間のいいね数の多い順に並べ替え

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
 
   def index
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    #includesは一回でbookに紐づいている情報を取り出せるようにするためのもの、ここではbookに紐づいている:favorited_usersを一回で呼び出す
+    @books = Book.includes(:favorited_users).
+      sort {|a,b|
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }
     @book = Book.new
-    @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
-
   end
 
   def show
