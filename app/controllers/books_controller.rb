4b489e63d89c9f94301378c1ed14f1365bc383f7
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-06-25 12:55:15 +0000

    NoMethodError in Users#showが出たところまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,10 +32,8 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      if @book.save
-        flash[:notice] = "You have created book successfully."
-        redirect_to book_path(@book.id)
-      end
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id) 
     else
       @books = Book.all
       render :index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
 
   def index
     @users = User.all
@@ -9,6 +10,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @currrentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+       @currrentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
+
   end
 
   def edit
@@ -23,12 +42,10 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      if @user.update(user_params)
-        flash[:notice] = "You have updated user successfully."
-        redirect_to user_path(@user)
-      end
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user)
     else
-      render :edit
+    render :edit
     end
   end
 
