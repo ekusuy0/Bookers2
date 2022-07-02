0ebd89e613c9ce4d1ea7a2b0597ee74ced062395
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-07-02 12:51:42 +0000

    [Update] 投稿に5段階評価をつける

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,6 @@ import '@fortawesome/fontawesome-free/js/all'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_124709) do
+ActiveRecord::Schema.define(version: 2022_07_02_102447) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
