b0d461c524aa6be2667b1d5b3779c670a21ab4d9
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-07-04 10:29:21 +0000

    [Update] タグの検索機能の追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,17 +2,13 @@ class Tag < ApplicationRecord
   has_many :book_tags, dependent: :destroy, foreign_key: 'tag_id'
   has_many :books, through: :book_tags
 
+  scope :merge_books, -> (tags){ }
+
   def self.looks(search, word)
     if search == "perfect_match"
-      @tag = Tag.where("tag_name LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @tag = Tag.where("tag_name LIKE?", "#{word}%")
-    elsif search == "backward__match"
-      @tag = Tag.where("tag_name LIKE?", "%#{word}")
-    elsif search == "partial_match"
-      @tag = Tag.where("tag_name LIKE?", "%#{word}%")
-    else
-      @tag = Tag.all
+      @tag = Tag.where(tag_name: word)
     end
+
+    return @tag.inject(init = []) {|result, tag| result + tag.books}
   end
 end
